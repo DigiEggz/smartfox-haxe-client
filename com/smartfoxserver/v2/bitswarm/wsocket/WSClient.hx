26780f84d54d69fb46e36946889bd4df38d3ee4a
DigiEggz <anony.mouse.jpt1l@slmail.me>
2023-08-08 12:23:05 -0400

    Add changes from barisyild fork
    Disconnection fixes
    
    https://github.com/barisyild/smartfox-haxe-client/commit/c3d17d0e3327f2804e792d61052512fa4a443e6d

--- com/smartfoxserver/v2/bitswarm/wsocket/WSClient.hx
+++ com/smartfoxserver/v2/bitswarm/wsocket/WSClient.hx
@@ -21,14 +21,17 @@ class WSClient extends EventDispatcher
 	public function new(debug : Bool = false)
 	{
 		super();
+        
+        #if (target.threaded)
+			Lib.current.addEventListener(Event.ENTER_FRAME, onEnterFrame);
+		#end
 	}
 
 	private function get_connected() : Bool
 	{
 		if (ws == null)
-		{
 			return false;
-		}
+        
 		return _connected;
 	}
 
@@ -68,6 +71,8 @@ class WSClient extends EventDispatcher
 			}));
 		};
 		ws.onclose = function() {
+            if(!_connected)
+                return;
 			_connected = false;
 			dispatchEvent(new WSEvent(WSEvent.CLOSED, { }));
 		};
@@ -78,16 +83,12 @@ class WSClient extends EventDispatcher
 			});
 			dispatchEvent(wsEvt);
 		};
-
-		#if (target.threaded)
-			Lib.current.addEventListener(Event.ENTER_FRAME, onEnterFrame);
-		#end
 	}
 
 	private function onEnterFrame(e:Event):Void
 	{
-		//trace("onEnterFrame");
-		ws.process();
+        if(ws != null)
+		  ws.process();
 	}
 
 	public function send(binData : ByteArray) : Void
@@ -98,7 +99,8 @@ class WSClient extends EventDispatcher
 
 	public function close() : Void
 	{
-		ws.close();
+        _connected = false;
+        ws.close();
 	}
 }
 
