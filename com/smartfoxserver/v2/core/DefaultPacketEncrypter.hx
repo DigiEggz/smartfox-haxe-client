ca9d3c87702dacf35fc750fb212b200e017bb20f
DigiEggz <anony.mouse.jpt1l@slmail.me>
2023-08-08 12:23:04 -0400

    Add support for encrypted Zones
    When a Zone is set to "Use encryption", client will properly encrypt/decrypt packets after calling initCrypto() on the SmartFox instance.

--- com/smartfoxserver/v2/core/DefaultPacketEncrypter.hx
+++ com/smartfoxserver/v2/core/DefaultPacketEncrypter.hx
@@ -1,15 +1,5 @@
 package com.smartfoxserver.v2.core;
 
-/**
- * ...
- * @author vincent blanchet
- */
-
-import com.hurlant.crypto.Crypto;
-import com.hurlant.crypto.symmetric.ICipher;
-import com.hurlant.crypto.symmetric.IPad;
-import com.hurlant.crypto.symmetric.IVMode;
-import com.hurlant.crypto.symmetric.PKCS5;
 import com.smartfoxserver.v2.SmartFox;
 import com.smartfoxserver.v2.bitswarm.BitSwarmClient;
 import com.smartfoxserver.v2.protocol.serialization.DefaultObjectDumpFormatter;
@@ -21,42 +11,22 @@ import flash.utils.ByteArray;
 class DefaultPacketEncrypter implements IPacketEncrypter
 {
 	private var bitSwarm:BitSwarmClient;
-	private static var ALGORITHM:String = "aes-cbc";
-	
+
 	public function new(bitSwarm:BitSwarmClient)
 	{
 		this.bitSwarm = bitSwarm;
 	}
-	
+
 	public function encrypt(data:ByteArray):Void
 	{
-		var ck:CryptoKey = bitSwarm.cryptoKey;
-		
-		/*
-		trace("IV  : \n" + DefaultObjectDumpFormatter.hexDump(ck.iv))
-		trace("KEY : \n" + DefaultObjectDumpFormatter.hexDump(ck.key))
-		trace("DATA: \n" + DefaultObjectDumpFormatter.hexDump(data))
-		*/
-		var padding:IPad = new PKCS5();
-		
-		var cipher:ICipher = Crypto.getCipher(ALGORITHM,cast ck.key,cast padding);
-		var ivmode:IVMode = cast(cipher, IVMode);
-		ivmode.IV = cast ck.iv;
-		
-		cipher.encrypt(cast data);
+		// Write encrypted data to beginning of ByteArray
+		data.position = 0;
+		data.writeBytes(bitSwarm.cipher.encrypt(Mode.CBC, data));
 	}
-	
+
 	public function decrypt(data:ByteArray):Void
 	{
-		var ck:CryptoKey = bitSwarm.cryptoKey;
-			
-		var padding:IPad = new PKCS5();
-		
-		var cipher:ICipher = Crypto.getCipher(ALGORITHM, cast ck.key,cast padding);
-		var ivmode:IVMode = cast(cipher, IVMode);
-		ivmode.IV = cast ck.iv;
-		
-		cipher.decrypt(cast data);
+		// Continually write decrypted data into ByteArray
+		data.writeBytes(bitSwarm.cipher.decrypt(Mode.CBC, data));
 	}
 }
-	
--- com/smartfoxserver/v2/util/PasswordUtil.hx
+++ com/smartfoxserver/v2/util/PasswordUtil.hx
@@ -1,10 +1,7 @@
 package com.smartfoxserver.v2.util;
 
-import com.hurlant.crypto.hash.IHash;
-import com.hurlant.crypto.hash.MD5;
-import com.hurlant.util.Hex;
-
-import flash.utils.ByteArray<Dynamic>;
+import haxe.crypto.Md5;
+import haxe.io.Bytes;
 
 /**
  * Helper class for logging in with a pre-hashed password.
@@ -14,25 +11,21 @@ class PasswordUtil
 {
 	/**
 	 * Generates the MD5 hash of the user password.
-	 * 
+	 *
 	 * @param	pass	The plain text password.
-	 * 
+	 *
 	 * @return	The hashed password.
-	 * 
+	 *
 	 * @example	This example shows how you can login with a hashed password:
 	 *<listing version="3.0">
 	 * var userName:String="testName";
 	 * var userPass:String="testPass";
-	 * 
+	 *
 	 * var md5Pass:String=PasswordUtil.md5Password(userPass);
 	 * sfs.send(new LoginRequest(userName, md5Pass, sfs.config.zone));
 	 *</listing>
 	 */
-	public static function md5Password(pass:String):String
-	{
-		var hash:IHash=new MD5();
-		var data:ByteArray<Dynamic>=Hex.toArray(Hex.fromString(pass));
-		
-		return Hex.fromArray(hash.hash(data));
+	public static function md5Password(pass:String):String {
+		return Md5.encode(pass);
 	}
 }
